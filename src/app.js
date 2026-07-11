5be2c569823ef1f986ec2c4c7de83c90e1258e92
Ashish Pandey <ap1700080@gmail.com>
2026-07-11 18:54:13 +0530

    all the important validations for the project and validations of the data

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const User = require("./models/users")
 
 app.use(express.json()) // this middleware is converting the json object into the js object for all the  routes.
 
+// new user api .
 app.post("/signup" , async(req,res)=>{
     console.log(req.body);
 
@@ -18,8 +19,75 @@ app.post("/signup" , async(req,res)=>{
     catch(err){
         res.status(400).send("error saving the data"+ err.message);
     }
+})
 
 
+// get the user by the emailId.
+app.get("/user",async(req,res)=>{
+ const useremail = req.body.emailId
+ try{
+    const users = User.find({emailId:useremail})
+    if(users.length===0) {
+        res.status(400).send("user not found")
+    }
+    else{
+        send(users)
+    }
+ }
+ catch(err){
+    res.status(400).send("something went wrong")
+ }
+})
+
+// feed api to get all the users from the database to the frontend.
+app.get("/feed", async(req,res)=>{
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }
+    catch(err){
+        res.status(400).send("something went wrong")
+    }
+})
+
+//delete user by ID
+app.delete("/user", async(req,res)=>{
+    const userid = req.body.userId; // Just taking the userid of the sent data and saving in the variable. 
+    try{
+        const user = User.findByIdAndDelete(userid)
+        res.send("user deleted succesfully")
+    }
+    catch(err){
+        res.status(400).send("error occured..")
+    }
+})
+
+//update user
+app.patch("/user/:userid",async(req,res)=>{
+    const userid=req.params?.userid; //this userid is bieng sent in the object from the frontend. // now taking from the url itself
+    const data =req.body; //this is the data or the updated data which is bieng also send by the client and which is needed to be updated in the database
+    // firstly the user in the document is bieng finded by the userid and data is bieng pass which needed to be updated..
+
+    
+    try{
+        const ALLOWED_UPDATE = ["userid","Photourl","about","skills","age"]
+        const isUpdateAllowed = Object.keys(data).every(
+      (k) => ALLOWED_UPDATE.includes(k)
+    );
+
+    if(data?.body.skills.length>10){
+        throw new Error("skills are too much ")
+    }
+
+    if(!isUpdateAllowed){
+      throw new Error("Update not allowed")
+    }
+        await User.findByIdAndUpdate(userid,data,{runValidators: true})
+        res.send("user updated successfully")
+    }
+    catch(err){
+        res.status(400).send("error occured")
+    }
 })
 
 connectDB()
--- src/models/users.js
+++ src/models/users.js
@@ -1,24 +1,63 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
+        required: true
     },
     lastName: {
         type: String,
     },
     emailId: {
         type: String,
+        required: true,
+        unique : true,
+        lowercase: true,
+        trim : true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error ("Invalid email address")
+            }
+        }
     },
     password: {
         type: String,
+        required: true,
+        validate(value){
+        if(!validator.isStrongPassword(value)){
+            throw new Error("not enough strong password")
+        }
+        }
     },
     age: {
         type: Number,
+        min : 18 // minimum age should be 18 in the app.
     },
     gender: {
         type: String,
+        validate(value){
+        if(!["male","female","other"].includes(value)){
+            throw new Error("Gender Data is not Validated")
+        }
+        }
     },
+    Photourl:{
+        type:string,
+        validate(value){
+         if(!validator.isURL(value)){
+            throw new Error("photo is not an url")
+         }
+        }
+    },
+    about:{
+        type:string
+    },
+    skills:{
+      type:[String]
+    }
+},{
+    timestamps:true,
 });
 
 const User = new mongoose.model("user",userSchema);
